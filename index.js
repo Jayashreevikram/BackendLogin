226a2a5c8a9b0736806fd0a97abcf30167db63fa
Jayashreevikram <kvjayshree91@gmail.com>
2025-10-21 00:52:16 +0530

    updated

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.post("/login", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("✅ Server running on http://localhost:3000");
-});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
