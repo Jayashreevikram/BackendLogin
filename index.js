8861dfc98f19f93daf02f903d759a179ed29691b
Jayashreevikram <kvjayshree91@gmail.com>
2025-10-21 13:59:15 +0530

    updated

--- index.js
+++ index.js
@@ -23,3 +23,6 @@ app.post("/login", (req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
+app.get("/", (req, res) => {
+  res.send("Backend server is running!");
+});
